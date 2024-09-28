07346b87cd0dc4a3ff7588bca10222056e01fd1a
JAIGURU <jaiguru.l2022ai-ml@sece.ac.in>
2024-09-28 17:33:20 +0530

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
